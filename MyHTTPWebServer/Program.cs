9938dd4bf579410747c69f189c1e506cbecae1da
APeev89 <apostol.peev@gmail.com>
2022-02-28 21:59:10 +0200

    Implement a Server Functionality

--- MyHTTPWebServer/Program.cs
+++ MyHTTPWebServer/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var port = 8080;
+var serverListener = new TcpListener(ipAddress, port);
+serverListener.Start();
+
+Console.WriteLine($"Server started on port {port}");
+Console.WriteLine($"Listening for requests...");
+
+
+
+while (true)
+{
+    var connection = serverListener.AcceptTcpClient();
+
+    var netWorkStream = connection.GetStream();
+
+    string content = "Hello from the server!";
+    int contentLength = Encoding.UTF8.GetByteCount(content);
+
+    string response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{content}";
+
+    var responseBytes = Encoding.UTF8.GetBytes(response);
+
+    netWorkStream.Write(responseBytes);
+    connection.Close();
+}
