ee200bc619c7159ab8987b531e02ade4a6421b9c
APeev89 <apostol.peev@gmail.com>
2022-03-01 12:38:51 +0200

    Create class with status code and class Header

---
+++ MyHTTPWebServer/HTTP/Header.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHTTPWebServer.HTTP
+{
+    public class Header
+    {
+        public Header(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+
+        }
+
+        public string Name { get; init; }
+
+        public string Value { get; set; }
+    }
+}
