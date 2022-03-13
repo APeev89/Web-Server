19d5776ca4fa7c2ec98355b8d2359a35bde12aab
APeev89 <apostol.peev@gmail.com>
2022-03-13 16:12:09 +0200

    Create Controller class

--- MyHTTPWebServer/Controllers/Controller.cs
+++ MyHTTPWebServer/Controllers/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using MyHTTPWebServer.HTTP;
+using MyHTTPWebServer.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,20 @@ namespace MyHTTPWebServer.Controllers
 {
     public abstract class Controller
     {
-        protected Controller()
+        protected Controller(Request request)
         {
-
+            this.Request = request;
         }
 
-        public Request Request { get; private init; }
+        protected Request Request { get; private init; }
+
+        protected Response Text(string text) => new TextResponse(text);
+        protected Response Html(string text) => new HtmlResponse(text);
+        protected Response BadRequest() => new BadRequestResponse();
+        protected Response Unauthorized() => new UnauthorizedResponse();
+        protected Response NotFound() => new NotFoundResponse();
+        protected Response Redirect(string location) => new RedirectResponse(location);
+        protected Response File(string fileName) => new TextFileResponse(fileName);
+        
     }
 }
