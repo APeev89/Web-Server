3448e5a868cf63c19cdf694554db99914e6957c3
APeev89 <apostol.peev@gmail.com>
2022-03-13 16:57:37 +0200

    Create Routing Extensions

---
+++ MyHTTPWebServer/Controllers/RoutingTableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MyHTTPWebServer.HTTP;
+using MyHTTPWebServer.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHTTPWebServer.Controllers
+{
+    public static class RoutingTableExtensions
+    {
+
+        public static IRoutingTable MapGet<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, Response> controllerFunction)
+            where TController : Controller
+            => routingTable.MapGet(path, request => controllerFunction(
+                 CreateController<TController>(request)));
+
+        public static IRoutingTable MapPost<TController>(
+            this IRoutingTable routingTable,
+            string path,
+            Func<TController, Response> controllerFunction)
+            where TController : Controller
+            => routingTable.MapPost(path,request => controllerFunction(
+                CreateController<TController>(request)));
+    }
+}
+
