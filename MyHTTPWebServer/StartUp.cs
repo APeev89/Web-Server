ecf0c8784685cfaf7e8c796b4bb53a879509a3c7
APeev89 <apostol.peev@gmail.com>
2022-03-13 18:38:58 +0200

    Create Controllers with Actions for Login and Logout

--- MyHTTPWebServer/StartUp.cs
+++ MyHTTPWebServer/StartUp.cs
@@ -10,77 +10,24 @@ using System.Web;
 public class StartUp
 {
 
-
-    private const string LoginForm = @"<form action='/Login' method='POST'>
-   Username: <input type='text' name='Username'/>
-   Password: <input type='text' name='Password'/>
-   <input type='submit' value ='Log In' /> 
-</form>";
-
-    private const string Username = "user";
-    private const string Password = "user123";
     public static async Task Main()
-    =>await new HttpServer(routes => routes
-       .MapGet<HomeController>("/", c => c.Index())
-       .MapGet<HomeController>("/Redirect",c => c.Redirect())
-       .MapGet<HomeController>("/HTML", c => c.Html())
-       .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
-       .MapGet<HomeController>("/Content", c => c.Content())
-       .MapPost<HomeController>("/Content", c => c.DownloadContent())
-       .MapGet<HomeController>("/Cookies", c => c.Cookies())
-       .MapGet<HomeController>("/Session", c => c.Session()))
-       //.MapGet<HomeController>("/Login", new HtmlResponse(StartUp.LoginForm))
-       //.MapPost<HomeController>t("/Login", new HtmlResponse("", StartUp.LoginAction))
-       //.MapGet<HomeController>("/Logout", new HtmlResponse("", StartUp.LogoutAction))
-       //.MapGet<HomeController>("/UserProfile",new HtmlResponse("", StartUp.GetUserDataAction)))
+    => await new HttpServer(routes => routes
+        .MapGet<HomeController>("/", c => c.Index())
+        .MapGet<HomeController>("/Redirect", c => c.Redirect())
+        .MapGet<HomeController>("/HTML", c => c.Html())
+        .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+        .MapGet<HomeController>("/Content", c => c.Content())
+        .MapPost<HomeController>("/Content", c => c.DownloadContent())
+        .MapGet<HomeController>("/Cookies", c => c.Cookies())
+        .MapGet<HomeController>("/Session", c => c.Session())
+        .MapGet<UsersController>("/Login", c => c.Login())
+        .MapPost<UsersController>("/Login", c => c.LogInUser())
+        .MapGet<UsersController>("/Logout", c => c.Logout())
+        .MapGet<UsersController>("/UserProfile", c => c.GetUserData()))
       .Start();
-    
 
-    private static void GetUserDataAction(Request request, Response response)
-    {
-        if (request.Session.ContainsKey(Session.SessionUserKey))
-        {
-            response.Body = "";
-            response.Body += $"<h3>Currently logged-in user is with username '{Username}'</h3>";
-        }
-        else
-        {
-            response.Body = "";
-            response.Body += $"<h3>You should first log in " + 
-                "- <a href='/Login'>Login</a></h3>";
-        }
 
-    }
+ 
 
-    private static void LogoutAction(Request request, Response response)
-    {
-        request.Session.Clear();
-        response.Body = "";
-        response.Body += "<h3>Logged out successfully</h3>";
-    }
-
-    private static void LoginAction(Request request, Response response)
-    {
-        request.Session.Clear();
-
-        var bodyText = "";
-        var usernameMatches = request.Form["Username"] == StartUp.Username;
-        var passwordMatches = request.Form["Password"] == StartUp.Password;
-        if (usernameMatches && passwordMatches)
-        {
-            request.Session[Session.SessionUserKey] = "MyUserId";
-            response.Cookies.Add(Session.SessionCookieName, request.Session.Id);
-            bodyText = "<h3>Logged successfully!</h3>";
-        }
-        else
-        {
-            bodyText = StartUp.LoginForm;
-        }
-
-        response.Body = "";
-        response.Body += bodyText;
-    }
-
-    
 }
 
